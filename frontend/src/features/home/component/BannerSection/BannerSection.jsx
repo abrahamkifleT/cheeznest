f64499a266b976819dd16ce3c1e5c383fcbcb8bd
abraham <abrahamkifle1212@gmail.com>
2026-03-28 22:24:21 +0300

    feat: add BannerSection component to home page with responsive layout and styling

--- frontend/src/features/home/component/BannerSection/BannerSection.jsx
+++ frontend/src/features/home/component/BannerSection/BannerSection.jsx
@@ -7,14 +7,18 @@ const BannerSection = () => {
     <div className='banner-container'>
         <div className='banner-left'>
             <div className='banner-left-content'>
-              <h2></h2>
+              <h2 className='banner-left-title-1'>Best Quality</h2>
+              <h2 className='banner-left-title-2'>Four Cheese Heaven</h2>
+              <a href="" className='banner-left-button'>Order Now</a>
             </div>
             <img src={BannerImg} alt="Banner Left" />
         </div>
 
         <div className='banner-right'>
            <div className='banner-right-content'>
-
+              <h2>Premium Quality</h2>
+              <h2>Meat Lover's Pizzas</h2>
+              <a href="">Order Now</a>
            </div>
 
            <img src={BannerImg} alt="Banner Right" />
