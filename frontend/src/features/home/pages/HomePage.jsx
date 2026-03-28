40693f601f6a6b81e3cd37cef460e85c0f1f687f
abraham <abrahamkifle1212@gmail.com>
2026-03-28 21:45:46 +0300

    feat: initialize homepage layout and configure base html entry point

--- frontend/src/features/home/pages/HomePage.jsx
+++ frontend/src/features/home/pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../component/Navbar/Navbar'
 import HeroSection from '../component/HeroSection/HeroSection'
+import FeatureSection from '../component/FeatureSection/FeatureSection'
+import BannerSection from '../component/BannerSection/BannerSection'
 
 const HomePage = () => {
     return (
         <div>
             <Navbar />
             <HeroSection />
+            <FeatureSection />
+            <BannerSection />
         </div>
     )
 }
