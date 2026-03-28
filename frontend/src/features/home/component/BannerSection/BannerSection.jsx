5c2782f5ee0890fd0d938b0787900e0379a80a7d
abraham <abrahamkifle1212@gmail.com>
2026-03-28 22:28:28 +0300

    feat: add BannerSection component to home feature

--- frontend/src/features/home/component/BannerSection/BannerSection.jsx
+++ frontend/src/features/home/component/BannerSection/BannerSection.jsx
@@ -5,24 +5,24 @@ import BannerImg from '../../../../assets/website/imgi_9_Group-1000012600-1.webp
 const BannerSection = () => {
   return (
     <div className='banner-container'>
-        <div className='banner-left'>
-            <div className='banner-left-content'>
-              <h2 className='banner-left-title-1'>Best Quality</h2>
-              <h2 className='banner-left-title-2'>Four Cheese Heaven</h2>
-              <a href="" className='banner-left-button'>Order Now</a>
-            </div>
-            <img src={BannerImg} alt="Banner Left" />
+      <div className='banner-left'>
+        <div className='banner-left-content'>
+          <h2 className='banner-left-title-1'>Best Quality</h2>
+          <h2 className='banner-left-title-2'>Four Cheese Heaven</h2>
+          <a href="" className='banner-left-button'><span>Order Now</span></a>
+        </div>
+        <img src={BannerImg} alt="Banner Left" />
+      </div>
+
+      <div className='banner-right'>
+        <div className='banner-right-content'>
+          <h2>Premium Quality</h2>
+          <h2>Meat Lover's Pizzas</h2>
+          <a href="">Order Now</a>
         </div>
 
-        <div className='banner-right'>
-           <div className='banner-right-content'>
-              <h2>Premium Quality</h2>
-              <h2>Meat Lover's Pizzas</h2>
-              <a href="">Order Now</a>
-           </div>
-
-           <img src={BannerImg} alt="Banner Right" />
-        </div>
+        <img src={BannerImg} alt="Banner Right" />
+      </div>
     </div>
   )
 }
