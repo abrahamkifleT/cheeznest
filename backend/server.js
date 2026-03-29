2672e2c1e2cb6abebb9090f1aa1ada459d7365cc
abraham <abrahamkifle1212@gmail.com>
2026-03-29 10:19:34 +0300

    feat: initialize backend project with Express server and basic configuration

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+// middleware
+
+app.use(express.json())
+
+
+// routes
+app.use("/api/v1/product", productRoutes)
+
+// server start
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
